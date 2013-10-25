2e197bf00f30d4861e7fdf2e671952c12ccfeaa6
Peter Cimermancic <peter.cimermancic@gmail.com>
2013-10-25 14:19:59 -0700

    added linear term to sigmoid XL-distance restraint

--- src/SigmoidRestraintSphere.cpp
+++ src/SigmoidRestraintSphere.cpp
@@ -18,10 +18,10 @@ SigmoidRestraintSphere::SigmoidRestraintSphere(IMP::kernel::Model *m,
                           IMP::kernel::ParticleIndexAdaptor p1,
                           IMP::kernel::ParticleIndexAdaptor p2,
                           double inflection, double slope, 
-                          double amplitude, std::string name):                  
+                          double amplitude, double line_slope, std::string name):                  
                           Restraint(m, name), 
                           p1_(p1),p2_(p2),inflection_(inflection),
-                          slope_(slope),amplitude_(amplitude) {  }                       
+                          slope_(slope),amplitude_(amplitude),line_slope_(line_slope) {  }                       
 
 
 double SigmoidRestraintSphere::
@@ -31,7 +31,13 @@ double SigmoidRestraintSphere::
     core::XYZR d1(get_model(), p1_);
     core::XYZR d2(get_model(), p2_);
 	double dist = IMP::core::get_distance(d1,d2);
-    double score= amplitude_*(1.0/(1.0+std::exp(-(dist-inflection_)/slope_)));
+    
+    double linear_contribution=0.0;
+    if (dist>inflection_){
+        linear_contribution=(dist-inflection_)*line_slope_;
+    }
+
+    double score= amplitude_*(1.0/(1.0+std::exp(-(dist-inflection_)/slope_)))+linear_contribution;
     if (accum){};
     
     return score;

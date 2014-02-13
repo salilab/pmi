4669cfca15419ae03181d04f76937efd4f7aafb1
Yannick Spill <yannick@salilab.org>
2014-02-13 17:07:54 +0100

    fix sampling of tau

--- pyext/src/restraints/saxs.py
+++ pyext/src/restraints/saxs.py
@@ -8,9 +8,9 @@ import IMP.container
 
 class SAXSISDRestraint():
 
-    import IMP.saxs 
-    import IMP.isd 
-    import IMP.isd2 
+    import IMP.saxs
+    import IMP.isd
+    import IMP.isd2
     import IMP.pmi.tools
 
     def __init__(self, representation,profile,resolution=0,weight=1,
@@ -20,8 +20,7 @@ class SAXSISDRestraint():
         self.label = "None"
         self.rs = IMP.RestraintSet(self.m, 'saxs')
 
-        self.sigmamaxtrans = 0.05
-        self.gammamaxtrans = 0.05
+        self.taumaxtrans = 0.05
         self.prof = IMP.saxs.Profile(profile)
 
         self.atoms=IMP.pmi.tools.select(representation,resolution=resolution)
@@ -35,7 +34,7 @@ class SAXSISDRestraint():
                       ).get_particle()
 
         # tau nuisance, optimized
-        self.tau = IMP.pmi.tools.SetupNuisance(self.m, .1, 0., None, True
+        self.tau = IMP.pmi.tools.SetupNuisance(self.m, 1., 0., None, False,
                       ).get_particle()
 
         #c1 and c2, optimized
@@ -75,6 +74,7 @@ class SAXSISDRestraint():
     def optimize_sigma(self):
         """set sigma to the value that maximizes its conditional likelihood"""
         from math import sqrt
+        self.m.update()
         sigma2hat = self.saxs.get_sigmasq_scale_parameter() \
                         /(self.saxs.get_sigmasq_shape_parameter()+1)
         IMP.isd.Scale(self.sigma).set_scale(sqrt(sigma2hat))
@@ -82,16 +82,51 @@ class SAXSISDRestraint():
     def optimize_gamma(self):
         """set gamma to the value that maximizes its conditional likelihood"""
         from math import exp
+        self.m.update()
         gammahat = exp(self.saxs.get_loggamma_variance_parameter()
                 * self.saxs.get_loggamma_jOg_parameter())
         IMP.isd.Scale(self.gamma).set_scale(gammahat)
 
+    def logspace(self,a,b,num=100):
+        """mimick numpy's logspace function"""
+        for i in xrange(num):
+            val = a + float(b-a)/float(num-1) * i
+            yield 10**val
+
+    def optimize_tau(self,ltaumin=-2,ltaumax=3,npoints=100):
+        from math import log
+        import IMP.atom
+        values=[]
+        self.m.update()
+        IMP.atom.write_pdb(self.atoms,'tauvals.pdb')
+        fl=open('tauvals.txt','w')
+        for tauval in self.logspace(ltaumin,ltaumax,npoints):
+            IMP.isd.Scale(self.tau).set_scale(tauval);
+            try:
+                values.append((self.m.evaluate(False), tauval))
+            except:
+                pass
+            fl.write('%G %G\n' % (values[-1][1],values[-1][0]))
+        values.sort()
+        ltcenter = log(values[0][1])/log(10)
+        spacing = (ltaumax-ltaumin)/float(npoints)
+        values=[]
+        for tauval in self.logspace(ltcenter-2*spacing, ltcenter+2*spacing,
+                npoints):
+            IMP.isd.Scale(self.tau).set_scale(tauval);
+            values.append((self.m.evaluate(False), tauval))
+            fl.write('%G %G\n' % (values[-1][1],values[-1][0]))
+        values.sort()
+        IMP.isd.Scale(self.tau).set_scale(values[0][1]);
+
     def draw_sigma(self):
         """draw 1/sigma2 from gamma distribution"""
+        self.m.update()
         self.saxs.draw_sigma()
 
     def draw_gamma(self):
         """draw gamma from lognormal distribution"""
+        self.m.update()
         self.saxs.draw_gamma()
 
     def update_covariance_matrix(self):
@@ -112,7 +147,6 @@ class SAXSISDRestraint():
                 fl.write('%G ' % i)
             fl.write('\n')
 
-
     def get_gamma_value(self):
         return self.gamma.get_scale()
 
@@ -123,18 +157,13 @@ class SAXSISDRestraint():
         self.m.add_restraint(self.rs)
         self.m.add_restraint(self.rs2)
 
-    def set_gammamaxtrans(self, gammamaxtrans):
-        self.gammamaxtrans = gammamaxtrans
-
-    def set_sigmamaxtrans(self, sigmamaxtrans):
-        self.sigmamaxtrans = sigmamaxtrans
+    def set_taumaxtrans(self, taumaxtrans):
+        self.taumaxtrans = taumaxtrans
 
     def get_particles_to_sample(self):
         ps = {}
-        ps["Nuisances_SAXSISDRestraint_Sigma_" +
-            self.label] = ([self.sigma], self.sigmamaxtrans)
-        ps["Nuisances_SAXSISDRestraint_Gamma_" +
-            self.label] = ([self.gamma], self.gammamaxtrans)
+        #ps["Nuisances_SAXSISDRestraint_Tau_" +
+        #    self.label] = ([self.tau], self.taumaxtrans)
         return ps
 
     def get_output(self):
@@ -148,6 +177,8 @@ class SAXSISDRestraint():
         output["SAXSISDRestraint_Prior_" + self.label] = str(score2)
         output["SAXSISDRestraint_Sigma_" +
                self.label] = str(self.sigma.get_scale())
+        output["SAXSISDRestraint_Tau_" +
+               self.label] = str(self.tau.get_scale())
         output["SAXSISDRestraint_Gamma_" +
                self.label] = str(self.gamma.get_scale())
         return output

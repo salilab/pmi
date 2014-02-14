940d455c1c49e76fb0f81c2cea8bdd88cbf7a0a6
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2014-02-13 22:10:40 -0800

    fixed bug in ReplicaExchange macro close #25

--- pyext/src/macros.py
+++ pyext/src/macros.py
@@ -95,6 +95,11 @@ class ReplicaExchange0():
 
       myindex=rex.get_my_index()
       self.output_objects.append(rex)
+      
+      # must reset the minimum and maximum temperature due to the 
+      # different binary length of rem.get_my_parameter and python float
+      self.vars["replica_exchange_minimum_temperature"]=rex.rem.get_my_parameter("temp")[0]
+      self.vars["replica_exchange_maximum_temperature"]=rex.rem.get_my_parameter("temp")[-1]
 
       sw = IMP.pmi.tools.Stopwatch()
       self.output_objects.append(sw)
@@ -248,6 +253,11 @@ class ReplicaExchange1():
 
       myindex=rex.get_my_index()
       self.output_objects.append(rex)
+      
+      # must reset the minimum and maximum temperature due to the 
+      # different binary length of rem.get_my_parameter and python float
+      self.vars["replica_exchange_minimum_temperature"]=rex.rem.get_my_parameter("temp")[0]
+      self.vars["replica_exchange_maximum_temperature"]=rex.rem.get_my_parameter("temp")[-1]
 
       sw = IMP.pmi.tools.Stopwatch()
       self.output_objects.append(sw)

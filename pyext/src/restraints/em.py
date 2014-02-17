117d1f61c3d95719c5da7822afd1ebad117ae9e8
Charles Greenberg <cgreen@salilab.org>
2014-02-17 09:51:58 -0800

    adding more options to em restraint

--- pyext/src/restraints/em.py
+++ pyext/src/restraints/em.py
@@ -9,7 +9,7 @@ import IMP.container
 
 class GaussianEMRestraint():
 
-    def __init__(self, densities, target_fn,target_mass=1.0):
+    def __init__(self, densities, target_fn,cutoff_dist_for_container=10.0,target_mass=1.0,radii_mult_factor=1.0):
         global sys, tools
         import sys
         import IMP.isd_emxl
@@ -24,7 +24,6 @@ class GaussianEMRestraint():
         self.sigmamin = 1.0
         self.sigmamax = 100.0
         self.sigmainit = 2.0
-        self.cutoff_dist_for_container = 10.0
         self.tabexp = False
         self.label="None"
 
@@ -33,7 +32,7 @@ class GaussianEMRestraint():
         target_ps = []
         IMP.isd_emxl.gmm_tools.decorate_gmm_from_text(target_fn, target_ps, self.m)
         for p in target_ps:
-            rmax=sqrt(max(IMP.core.Gaussian(p).get_variances()))
+            rmax=sqrt(max(IMP.core.Gaussian(p).get_variances()))*radii_mult_factor
             IMP.core.XYZR.setup_particle(p,rmax)
             mp=IMP.atom.Mass(p)
             mp.set_mass(mp.get_mass()*target_mass)
@@ -55,7 +54,7 @@ class GaussianEMRestraint():
                                                                      IMP.get_indexes(model_ps),
                                                                      IMP.get_indexes(target_ps),
                                                                      self.sigmaglobal.get_particle().get_index(),
-                                                                     self.cutoff_dist_for_container,
+                                                                     cutoff_dist_for_container,
                                                                      False, False)
         print 'done EM setup'
         self.rs = IMP.RestraintSet(self.m, 'GaussianEMRestraint')

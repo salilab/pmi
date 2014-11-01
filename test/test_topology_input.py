ef59f74aa58d231b5b8611408d4d47f6f20f822c
saltzberg <saltzberg.salilab.org>
2014-10-31 19:37:59 -0700

    Added a build_model() step to the end of topology_input test

--- test/test_topology_input.py
+++ test/test_topology_input.py
@@ -10,3 +10,9 @@ t=IMP.pmi.topology.topology_io.TopologyReader(topology_file)
 
 for c in t.component_list:
     print c.name, c.pdb_file, c.residue_range, c.gmm_file
+
+
+m = IMP.Model()
+simo = IMP.pmi.representation.Representation(m,upperharmonic=True,disorderedlength=False)
+bm=IMP.pmi.macros.BuildModel1(simo)
+bm.build_model(component_topologies=t.component_list[6:8])

e7f00f20c83db216bc7385d300c202c9be54cd7c
Ben Webb <ben@salilab.org>
2014-11-06 16:41:17 -0800

    Speed up test.

--- test/test_set_coordinates_from_rmf.py
+++ test/test_set_coordinates_from_rmf.py
@@ -1,62 +1,56 @@
+import os
 import IMP
-import IMP.core
-import IMP.base
-import IMP.algebra
-import IMP.atom
-import IMP.container
+import IMP.test
 
 import IMP.pmi.restraints.stereochemistry
 import IMP.pmi.representation
 import IMP.pmi.tools
-import IMP.pmi.samplers
 import IMP.pmi.output
 
-# input parameter
+class Tests(IMP.test.TestCase):
+    def test_set_from_rmf(self):
+        """Test setting coordinates from RMF"""
 
-pdbfile = IMP.pmi.get_data_path("1WCM.pdb")
-fastafile = IMP.pmi.get_data_path("1WCM.fasta.txt")
+        # input parameter
+        pdbfile = self.get_input_file_name("mini.pdb")
+        fastafile = self.get_input_file_name("mini.fasta")
 
-components = ["Rpb1", "Rpb2", "Rpb3", "Rpb4"]
+        components = ["Rpb1", "Rpb2"]
 
-chains = "ABCDEFGHIJKL"
+        chains = "AB"
 
-colors = [0., 0.5,  0.75, 1.0]
+        colors = [0., 1.0]
 
-beadsize = 20
+        beadsize = 20
 
-fastids = IMP.pmi.tools.get_ids_from_fasta_file(fastafile)
+        fastids = IMP.pmi.tools.get_ids_from_fasta_file(fastafile)
 
-m = IMP.Model()
-simo = IMP.pmi.representation.SimplifiedModel(m)
+        m = IMP.Model()
+        simo = IMP.pmi.representation.SimplifiedModel(m)
 
 
-simo.add_component_name("Rpb1", color=colors[0])
-simo.add_component_sequence("Rpb1", fastafile, id=fastids[0])
-simo.autobuild_model("Rpb1", pdbfile, "A",
-                     resolutions=[1], missingbeadsize=beadsize)
-simo.setup_component_sequence_connectivity("Rpb1", 1)
+        simo.add_component_name("Rpb1", color=colors[0])
+        simo.add_component_sequence("Rpb1", fastafile, id=fastids[0])
+        simo.autobuild_model("Rpb1", pdbfile, "A",
+                             resolutions=[1], missingbeadsize=beadsize)
+        simo.setup_component_sequence_connectivity("Rpb1", 1)
 
-simo.add_component_name("Rpb2", color=colors[1])
-simo.add_component_sequence("Rpb2", fastafile, id=fastids[1])
-simo.autobuild_model("Rpb2", pdbfile, "B",
-                     resolutions=[10], beadsize=beadsize)
-simo.setup_component_sequence_connectivity("Rpb2", 1)
+        simo.add_component_name("Rpb2", color=colors[1])
+        simo.add_component_sequence("Rpb2", fastafile, id=fastids[1])
+        simo.autobuild_model("Rpb2", pdbfile, "B",
+                             resolutions=[10], beadsize=beadsize)
+        simo.setup_component_sequence_connectivity("Rpb2", 1)
 
-simo.add_component_name("Rpb3", color=colors[2])
-simo.add_component_sequence("Rpb3", fastafile, id=fastids[2])
-simo.autobuild_model("Rpb3", pdbfile, "C",
-                     resolutions=[0], beadsize=beadsize)
-simo.setup_component_sequence_connectivity("Rpb3", 1)
+        output = IMP.pmi.output.Output()
+        output.init_rmf("test_set_coordinates_from_rmf.rmf3", [simo.prot])
+        output.write_rmf("test_set_coordinates_from_rmf.rmf3")
+        output.close_rmf("test_set_coordinates_from_rmf.rmf3")
 
-simo.add_component_name("Rpb4", color=colors[3])
-simo.add_component_sequence("Rpb4", fastafile, id=fastids[3])
-simo.autobuild_model("Rpb4", pdbfile, "D",
-                     resolutions=[0, 1], beadsize=beadsize)
-simo.setup_component_sequence_connectivity("Rpb4", 1)
+        simo.set_coordinates_from_rmf("Rpb1",
+                                      "test_set_coordinates_from_rmf.rmf3", 0)
 
-output = IMP.pmi.output.Output()
-output.init_rmf("test_set_coordinates_from_rmf.rmf3", [simo.prot])
-output.write_rmf("test_set_coordinates_from_rmf.rmf3")
-output.close_rmf("test_set_coordinates_from_rmf.rmf3")
+        os.unlink("test_set_coordinates_from_rmf.rmf3")
 
-simo.set_coordinates_from_rmf("Rpb1", "test_set_coordinates_from_rmf.rmf3", 0)
+
+if __name__ == '__main__':
+    IMP.test.main()

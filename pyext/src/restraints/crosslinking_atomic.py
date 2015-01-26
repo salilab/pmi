75e9956666c5907611bb6148f1bb6f66ae6b88e7
Charles Greenberg <cgreen@salilab.org>
2015-01-25 17:33:25 -0800

    easier to visualize xlink violations

--- pyext/src/restraints/crosslinking_atomic.py
+++ pyext/src/restraints/crosslinking_atomic.py
@@ -13,7 +13,7 @@ import IMP.isd
 import IMP.pmi.sampling_tools as sampling_tools
 from collections import defaultdict
 import os.path
-
+import string
 def setup_nuisance(m,rs,
                    init_val,
                    min_val_nuis,
@@ -282,7 +282,9 @@ class AtomicCrossLinkMSRestraint(object):
     def plot_violations(self,out_prefix,
                         max_prob_for_violation=0.1,
                         min_dist_for_violation=1e9,
-                        coarsen=False):
+                        coarsen=False,
+                        limit_to_chains=None,
+                        exclude_chains=''):
         """Create CMM files, one for each state, of all xinks.
         will draw in GREEN if non-violated in all states (or if only one state)
         will draw in PURPLE if non-violated only in a subset of states (draws nothing elsewhere)
@@ -293,65 +295,51 @@ class AtomicCrossLinkMSRestraint(object):
         @param max_prob_for_violation It's a violation if the probability is below this
         @param min_dist_for_violation It's a violation if the min dist is above this
         @param coarsen                Use CA positions
+        @param limit_to_chains        Try to visualize just these chains
+        @param exclude_to_chains        Try to NOT visualize these chains
         """
         print('going to calculate violations and plot CMM files')
-        all_dists=[]
+        all_stats = self.get_best_stats()
+        all_dists = [s["low_dist"] for s in all_stats]
 
         # prepare one output file per state
         out_fns=[]
         out_nvs=[]
+        state_info=[]
         for nstate in range(self.nstates):
             outf=open(out_prefix+str(nstate)+'.cmm','w')
             outf.write('<marker_set name="xlinks_state%i"> \n' % nstate)
             out_fns.append(outf)
             out_nvs.append(0)
+            print('will limit to',limit_to_chains)
+            print('will exclude',exclude_chains)
+            state_info.append(self.get_best_stats(nstate,
+                                                  limit_to_chains,
+                                                  exclude_chains))
 
-        # for each crosslink, evaluate probability and lowest distance for each state
         for nxl in range(self.rs.get_number_of_restraints()):
-            print(nxl)
-            xl=IMP.isd.AtomicCrossLinkMSRestraint.get_from(self.rs.get_restraint(nxl))
-            best_contr_per_state=[[1e6,None] for i in range(self.nstates)] # [low dist, idx low contr]
-            ncontr_per_state=[[] for i in range(self.nstates)]     # [list of the ncontr in each state]
-
-            # get best contributions and probabilities for each state
-            for ncontr in range(xl.get_number_of_contributions()):
-                idxs = xl.get_contribution(ncontr)
-                nstate = IMP.atom.get_state_index(IMP.atom.Atom(self.mdl,idxs[0]))
-                ncontr_per_state[nstate].append(ncontr)
-                idx1,idx2,dist=self._get_contribution_info(xl,ncontr,use_CA=coarsen)
-                if dist<best_contr_per_state[nstate][0]*0.9:
-                    if best_contr_per_state[nstate][1] is not None:
-                        c1=IMP.atom.get_chain_id(IMP.atom.Atom(self.mdl,idx1))
-                        c2=IMP.atom.get_chain_id(IMP.atom.Atom(self.mdl,idx2))
-                        if c1==c2:
-                            continue
-                    best_contr_per_state[nstate] = [dist,ncontr]
-            prob_per_state = [xl.evaluate_for_contributions(cr,None) for cr in ncontr_per_state]
-            prob_global = xl.unprotected_evaluate(None)
-            all_dists.append(min(best_contr_per_state)[0])
-
-            # now check each state and see how many pass
+            # for this XL, check which states passed
             npass=[]
             nviol=[]
             for nstate in range(self.nstates):
-                prob = prob_per_state[nstate]
-                idx1,idx2,low_dist=self._get_contribution_info(xl,best_contr_per_state[nstate][1],
-                                                               use_CA=coarsen)
+                prob = state_info[nstate][nxl]["prob"]
+                low_dist = state_info[nstate][nxl]["low_dist"]
                 if prob<max_prob_for_violation or low_dist>min_dist_for_violation:
                     nviol.append(nstate)
                 else:
                     npass.append(nstate)
 
-            # special case when all pass or all fail
+            # special coloring when all pass or all fail
             all_pass=False
             all_viol=False
-
             if len(npass)==self.nstates:
                 all_pass=True
             elif len(nviol)==self.nstates:
                 all_viol=True
-            print(xl,'prob:',prob_global,'prob per state: ',prob_per_state,'best dists',best_contr_per_state,all_viol)
+
             # finally, color based on above info
+            print(nxl,'state dists:',[state_info[nstate][nxl]["low_dist"] for nstate in range(self.nstates)],
+                  'viol states:',nviol,'all viol?',all_viol)
             for nstate in range(self.nstates):
                 if all_pass:
                     r=0.365; g=0.933; b=0.365;
@@ -362,12 +350,9 @@ class AtomicCrossLinkMSRestraint(object):
                         continue
                     else:
                         r=0.9; g=0.34; b=0.9;
-                idx1,idx2,low_dist=self._get_contribution_info(xl,best_contr_per_state[nstate][1],
-                                                               use_CA=coarsen)
-                c1=IMP.core.XYZ(self.mdl,idx1).get_coordinates()
-                c2=IMP.core.XYZ(self.mdl,idx2).get_coordinates()
-                a1=IMP.atom.Atom(self.mdl,idx1)
-                a2=IMP.atom.Atom(self.mdl,idx2)
+                pp = state_info[nstate][nxl]["low_pp"]
+                c1=IMP.core.XYZ(self.mdl,pp[0]).get_coordinates()
+                c2=IMP.core.XYZ(self.mdl,pp[1]).get_coordinates()
 
                 outf = out_fns[nstate]
                 nv = out_nvs[nstate]
@@ -397,27 +382,67 @@ class AtomicCrossLinkMSRestraint(object):
                                         IMP.core.XYZ(self.mdl,idx2).get_coordinates())
         return idx1,idx2,dist
 
-    def print_stats(self):
-        print("XL restraint statistics\n<num> <prob> <bestdist> <sig1> <sig2> <is_viol>")
+    def get_best_stats(self,limit_to_state=None,limit_to_chains=None,exclude_chains=''):
+        ''' return the probability, best distance, two coords, and possibly the psi for each xl
+        @param limit_to_state Only examine contributions from one state
+        @param limit_to_chains Returns the particles for certain "easy to visualize" chains
+        @param exclude_chains  Even if you limit, don't let one end be in this list.
+                               Only works if you also limit chains
+        '''
+        ret = []
         for nxl in range(self.rs.get_number_of_restraints()):
+            this_info = {}
             xl=IMP.isd.AtomicCrossLinkMSRestraint.get_from(self.rs.get_restraint(nxl))
-            prob = xl.unprotected_evaluate(None)
-            if prob<0.05:
-                is_viol=1
-            else:
-                is_viol=0
             low_dist=1e6
-            low_contr=None
+            low_contr = None
+            low_pp = None
+            state_contrs=[]
+            low_pp_lim = None
+            low_dist_lim = 1e6
             for contr in range(xl.get_number_of_contributions()):
                 pp = xl.get_contribution(contr)
+                if limit_to_state is not None:
+                    nstate = IMP.atom.get_state_index(IMP.atom.Atom(self.mdl,pp[0]))
+                    if nstate!=limit_to_state:
+                        continue
+                    state_contrs.append(contr)
                 dist = IMP.core.get_distance(IMP.core.XYZ(self.mdl,pp[0]),
                                              IMP.core.XYZ(self.mdl,pp[1]))
+                if limit_to_chains is not None:
+                    c1 = IMP.atom.get_chain_id(IMP.atom.Atom(self.mdl,pp[0]))
+                    c2 = IMP.atom.get_chain_id(IMP.atom.Atom(self.mdl,pp[1]))
+                    if (c1 in limit_to_chains or c2 in limit_to_chains) and (
+                            c1 not in exclude_chains and c2 not in exclude_chains):
+                        if dist<low_dist_lim:
+                            print('may store',nxl,c1,c2)
+                            low_dist_lim = dist
+                            low_pp_lim = pp
                 if dist<low_dist:
-                    low_dist=dist
-                    low_contr=contr
-            #dist,sig1,sig2,psi = xl.get_contribution_scores(low_contr)
+                    low_dist = dist
+                    low_contr = contr
+                    low_pp = pp
+            if limit_to_state is not None:
+                this_info["prob"] = xl.evaluate_for_contributions(state_contrs,None)
+            else:
+                this_info["prob"] = xl.unprotected_evaluate(None)
+            if limit_to_chains is not None:
+                this_info["low_pp"] = low_pp_lim
+            else:
+                this_info["low_pp"] = low_pp
+
+            this_info["low_dist"] = low_dist
+            if not self.one_psi:
+                pval = IMP.isd.Scale(self.mdl,xl.get_psi()).get_scale()
+                this_info["psi"] = pval
+            ret.append(this_info)
+        return ret
+
+    def print_stats(self):
+        #print("XL restraint statistics\n<num> <prob> <bestdist> <sig1> <sig2> <is_viol>")
+        stats = get_best_stats()
+        for nxl,s in enumerate(stats):
             #print('%i %.4f %.4f %.4f %.4f %i'%(nxl,prob,low_dist,sig1,sig2,is_viol))
-            print(low_dist)
+            print(s["low_dist"])
 
 
     def get_output(self):
@@ -428,8 +453,6 @@ class AtomicCrossLinkMSRestraint(object):
         output["AtomicXLRestraint" + self.label] = str(score)
 
         ### HACK to make it easier to see the few sigmas
-        #output["AtomicXLRestraint_sig_low"] = self.sig_low.get_scale()
-        #output["AtomicXLRestraint_sig_high"] = self.sig_high.get_scale()
         output["AtomicXLRestraint_sigma"] = self.sigma.get_scale()
         output["AtomicXLRestraint_priors"] = self.rs_nuis.unprotected_evaluate(None)
         if self.one_psi:
@@ -438,29 +461,13 @@ class AtomicCrossLinkMSRestraint(object):
 
         # count distances above length
         bad_count=0
-        for nxl in range(self.rs.get_number_of_restraints()):
-            xl=IMP.isd.AtomicCrossLinkMSRestraint.get_from(self.rs.get_restraint(nxl))
-            prob = xl.unprotected_evaluate(None)
-            if prob<0.1:
+        stats = get_best_stats()
+        for nxl,s in enumerate(stats):
+            if s['low_dist']<20.0:
                 bad_count+=1
-            low_dist=1e6
-            low_contr=None
-            for contr in range(xl.get_number_of_contributions()):
-                pp = xl.get_contribution(contr)
-                dist = IMP.core.get_distance(IMP.core.XYZ(self.mdl,pp[0]),
-                                             IMP.core.XYZ(self.mdl,pp[1]))
-                if dist<low_dist:
-                    low_dist=dist
-                    low_contr=contr
-
-            output["AtomicXLRestraint_%i_%s"%(nxl,"Prob")]=str(prob)
-            output["AtomicXLRestraint_%i_%s"%(nxl,"BestDist")]=str(low_dist)
+            output["AtomicXLRestraint_%i_%s"%(nxl,"Prob")]=str(s['prob'])
+            output["AtomicXLRestraint_%i_%s"%(nxl,"BestDist")]=str(s['low_dist'])
             if not self.one_psi:
-                pval = IMP.isd.Scale(self.mdl,xl.get_psi()).get_scale()
-                output["AtomicXLRestraint_%i_%s"%(nxl,"psi")]=str(pval)
-            #dist,sig1,sig2,psi = xl.get_contribution_scores(low_contr)
-            #output["AtomicXLRestraint_%i_%s"%(nxl,"Sig1")]=str(sig1)
-            #output["AtomicXLRestraint_%i_%s"%(nxl,"Sig2")]=str(sig2)
-
+                output["AtomicXLRestraint_%i_%s"%(nxl,"psi")]=str(s['psi'])
         output["AtomicXLRestraint_NumViol"] = str(bad_count)
         return output

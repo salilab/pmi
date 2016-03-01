71826b20b4b460b795e979c0da136aa23b7ae21c
Daniel Saltzberg <saltzberg@salilab.org>
2016-03-01 11:53:17 -0800

    remove extra code

--- pyext/src/restraints/em.py
+++ pyext/src/restraints/em.py
@@ -278,13 +278,6 @@ class GaussianEMRestraint(object):
                 self.em_root_hier.add_child(p)
         return self.em_root_hier
 
-    def add_target_density_to_state(self,state):
-        # Check to make sure this is a state:
-        if type(state) is IMP.pmi.topology.State:
-            state.get_hierarchy().add_child(self.get_density_as_hierarchy())
-        else:
-            raise Exception("Can only add a density to a PMI State object")
-
     def add_target_density_to_hierarchy(self,inp):
         ''' Can add a target GMM to a Hierarchy.
         For PMI2 a state object may also be passed'''

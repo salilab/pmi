Making RMSF calculation work for PMI2, and testing it. Mainly, adding correct selection in _get_residue_particle_index_map

pyext/src/analysis.py
@@ -641,8 +641,13 @@ class Precision(object):
                 self.structures_dictionary = self.comm.recv(source=0, tag=11)
 
     def _get_residue_particle_index_map(self,prot_name,structure,hier):
+        # Creates map from all particles to residue numbers
         residue_particle_index_map = []
-        s = IMP.atom.Selection(hier,molecules=[prot_name])
+        if IMP.pmi.get_is_canonical(hier):
+            s = IMP.atom.Selection(hier,molecules=[prot_name],
+                                   resolution=1)
+        else:
+            s = IMP.atom.Selection(hier,molecules=[prot_name])
         all_selected_particles = s.get_selected_particles()
         intersection = list(set(all_selected_particles) & set(structure))
         sorted_intersection = IMP.pmi.tools.sort_by_residues(intersection)
@@ -839,42 +844,41 @@ class Precision(object):
         @param set_plot_yaxis_range In case you need to change the plot
         """
         # get the centroid structure for the whole complex
-        centroid_index=self.get_precision(
-            structure_set_name,
-            structure_set_name,
-            outfile=None,
-            skip=skip)
+        centroid_index = self.get_precision(structure_set_name,
+                                            structure_set_name,
+                                            outfile=None,
+                                            skip=skip)
         if self.rank==0:
             for sel_name in self.protein_names:
                 self.selection_dictionary.update({sel_name:[sel_name]})
                 try:
-                    number_of_structures=len(self.structures_dictionary[structure_set_name][sel_name])
+                    number_of_structures = len(self.structures_dictionary[structure_set_name][sel_name])
                 except KeyError:
                     # that protein was not included in the selection
                     continue
-                rpim=self.residue_particle_index_map[sel_name]
-                outfile=outdir+"/rmsf."+sel_name+".dat"
-                of=open(outfile,"w")
-                residue_distances={}
-                residue_nblock={}
+                rpim = self.residue_particle_index_map[sel_name]
+                outfile = outdir+"/rmsf."+sel_name+".dat"
+                of = open(outfile,"w")
+                residue_distances = {}
+                residue_nblock = {}
                 for index in range(number_of_structures):
-                    distances=self._get_particle_distances(structure_set_name,
-                                                          structure_set_name,
-                                                          sel_name,
-                                                          centroid_index,index)
+                    distances = self._get_particle_distances(structure_set_name,
+                                                             structure_set_name,
+                                                             sel_name,
+                                                             centroid_index,index)
                     for nblock,block in enumerate(rpim):
                         for residue_number in block:
-                            residue_nblock[residue_number]=nblock
+                            residue_nblock[residue_number] = nblock
                             if residue_number not in residue_distances:
-                                residue_distances[residue_number]=[distances[nblock]]
+                                residue_distances[residue_number] = [distances[nblock]]
                             else:
                                 residue_distances[residue_number].append(distances[nblock])
 
-                residues=[]
-                rmsfs=[]
+                residues = []
+                rmsfs = []
                 for rn in residue_distances:
                     residues.append(rn)
-                    rmsf=np.std(residue_distances[rn])
+                    rmsf = np.std(residue_distances[rn])
                     rmsfs.append(rmsf)
                     of.write(str(rn)+" "+str(residue_nblock[rn])+" "+str(rmsf)+"\n")
 

test/medium_test_analysis2.py
@@ -7,18 +7,20 @@ from math import sqrt
 import itertools
 import os
 import glob
+import shutil
+import tempfile
 nicemodules = True
 try:
+    import numpy as np
     import scipy
     import sklearn
-except ImportError:
-    nicemodules = False
-if nicemodules:
     import IMP.pmi.analysis
     import IMP.pmi.io
     import IMP.pmi.dof
     import IMP.pmi.macros
     import IMP.pmi.restraints.basic
+except ImportError:
+    nicemodules = False
 
 def get_drms(coords0,coords1):
     total = 0.0
@@ -272,7 +274,7 @@ class AnalysisTest(IMP.test.TestCase):
         self.assertTrue(IMP.em.get_bounding_box(dmap2).get_contains(bbox2))
 
     def test_precision(self):
-        """Test correct calcluation of precision"""
+        """Test correct calcluation of precision and RMSF"""
         if not nicemodules:
             self.skipTest("missing scipy or sklearn")
 
@@ -293,7 +295,7 @@ class AnalysisTest(IMP.test.TestCase):
         fn00 = self.get_tmp_file_name('f00.txt')
         fn01 = self.get_tmp_file_name('f01.txt')
         fn11 = self.get_tmp_file_name('f11.txt')
-        pr.get_precision('set0','set0',fn00)
+        cn00 = pr.get_precision('set0','set0',fn00)
         pr.get_precision('set0','set1',fn01)
         pr.get_precision('set1','set1',fn11)
 
@@ -314,5 +316,14 @@ class AnalysisTest(IMP.test.TestCase):
             pdist11 = float(inf.readlines()[-1].strip().split()[-1])
             self.assertAlmostEqual(dist11,pdist11,places=2)
 
+        # check RMSF
+        rmsf_dir = tempfile.mkdtemp()
+        pr.get_rmsf('set0',outdir=rmsf_dir)
+        mean1 = coordslist0[cn00][0]
+        rmsf1 = np.std([IMP.algebra.get_distance(c[0],mean1) for c in coordslist0])
+        with open(os.path.join(rmsf_dir,'rmsf.Prot1.dat'),'r') as inf:
+            prmsf1 = float(inf.readline().split()[2])
+            self.assertAlmostEqual(rmsf1,prmsf1)
+        shutil.rmtree(rmsf_dir)
 if __name__ == '__main__':
     IMP.test.main()
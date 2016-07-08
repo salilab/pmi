f8e7cd9eb37feca1ce7b2d8c0a2ad350cf25190b
Ben Webb <ben@salilab.org>
2016-07-07 17:36:04 -0700

    Force only a single frame in test mode.

--- pyext/src/macros.py
+++ pyext/src/macros.py
@@ -379,7 +379,10 @@ class ReplicaExchange0(object):
         if myindex == 0:
             self.show_info()
         self.replica_exchange_object.set_was_used(True)
-        for i in range(self.vars["number_of_frames"]):
+        nframes = self.vars["number_of_frames"]
+        if self.test_mode:
+            nframes = 1
+        for i in range(nframes):
             for nr in range(self.vars["num_sample_rounds"]):
                 if sampler_md is not None:
                     sampler_md.optimize(self.vars["molecular_dynamics_steps"])

812fc5d3b4656b93524a1aacff90a55a3447b101
Daniel Saltzberg <saltzberg@salilab.org>
2017-04-05 15:04:37 -0700

    Add test for ProcessOutput
    Tests reading in the stat file and retrieving category keys, lines that pass a criteria and column values.

--- test/test_output.py
+++ test/test_output.py
@@ -19,6 +19,25 @@ class Tests(IMP.test.TestCase):
         self.assertAlmostEqual(center[1], 0., delta=1e-5)
         self.assertAlmostEqual(center[2], 0., delta=1e-5)
         os.unlink('test_output.pdb')
+        
+    def test_process_output(self):
+        import numpy
+        """test reading stat files"""
+        instat = self.get_input_file_name("./output1/stat.0.out")
+
+        po = IMP.pmi.output.ProcessOutput(instat)
+
+        categories = po.get_keys()
+        self.assertEqual(len(categories), 25)
+
+        criteria = [("rmf_frame_index", 5, "<")]
+        self.assertEqual(len(po.return_models_satisfying_criteria(criteria)), 11)
+
+        criteria = [("rmf_frame_index", 5, "<"), ('AtomicXLRestraint', 10.0, ">")]
+        self.assertEqual(len(po.return_models_satisfying_criteria(criteria)), 4)
+
+        vals = po.get_fields(["AtomicXLRestraint"])["AtomicXLRestraint"]
+        self.assertAlmostEqual(numpy.average(numpy.array(vals).astype(numpy.float)), 10.1270600392)
 
 if __name__ == '__main__':
     IMP.test.main()

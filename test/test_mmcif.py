02c702d6ab801a27c8f21d54070e714a00077572
Ben Webb <ben@salilab.org>
2017-05-19 12:42:39 -0700

    Add test for _assign_id().

--- test/test_mmcif.py
+++ test/test_mmcif.py
@@ -30,6 +30,30 @@ def make_dataset_dumper():
 
 class Tests(IMP.test.TestCase):
 
+    def test_assign_id(self):
+        """Test _assign_id utility function"""
+        class DummyObj(object):
+            def __init__(self, hashval):
+                self.hashval = hashval
+            def __eq__(self, other):
+                return self.hashval == other.hashval
+            def __hash__(self):
+                return self.hashval
+        seen_objs = {}
+        obj_by_id = []
+        obj1a = DummyObj(42) # obj1a and 1b are identical
+        obj1b = DummyObj(42)
+        obj2 = DummyObj(34)
+        obj3 = DummyObj(23) # obj3 already has an id
+        obj3.id = 'foo'
+        for obj in (obj1a, obj1b, obj2, obj3):
+            IMP.pmi.mmcif._assign_id(obj, seen_objs, obj_by_id)
+        self.assertEqual(obj1a.id, 1)
+        self.assertEqual(obj1b.id, 1)
+        self.assertEqual(obj2.id, 2)
+        self.assertEqual(obj3.id, 'foo')
+        self.assertEqual(obj_by_id, [obj1a, obj2])
+
     def test_software(self):
         """Test SoftwareDumper"""
         s = IMP.pmi.metadata.Software(name='test', classification='test code',

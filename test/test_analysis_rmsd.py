2416d85e81d1a82210df27ade45672facb81f753
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2017-06-28 18:10:10 +0200

    fixed test

--- test/test_analysis_rmsd.py
+++ test/test_analysis_rmsd.py
@@ -6,24 +6,25 @@ import IMP.pmi.topology
 import IMP.pmi.macros
 import IMP.pmi.analysis
 
-mdl = IMP.Model()
-reader = IMP.pmi.topology.TopologyReader('input/1mda/1mda_topology.dat',
-                                         pdb_dir =   'input/1mda/',
-                                         fasta_dir = 'input/1mda/',
-                                         gmm_dir =   'input/1mda/gmm')
-
-bs = IMP.pmi.macros.BuildSystem(mdl)
-bs.add_state(reader)
-hier, dof = bs.execute_macro(max_rb_trans=4.0, max_rb_rot=0.4, max_bead_trans=4.0, max_srb_trans=4.0,max_srb_rot=0.4)
-mols=IMP.pmi.tools.get_molecules(hier)
-
-
 
 
 class Tests(IMP.test.TestCase):
+    def setUp(self):
+        mdl = IMP.Model()
+	IMP.test.TestCase.setUp(self)
+	reader = IMP.pmi.topology.TopologyReader(self.get_input_file_name('1mda/1mda_topology.dat'),
+                                        	 pdb_dir =   self.get_input_file_name('1mda/'),
+                                        	 fasta_dir = self.get_input_file_name('1mda/'),
+                                        	 gmm_dir =   self.get_input_file_name('1mda/gmm'))
+
+	bs = IMP.pmi.macros.BuildSystem(mdl)
+	bs.add_state(reader)
+	self.hier, self.dof = bs.execute_macro(max_rb_trans=4.0, max_rb_rot=0.4, max_bead_trans=4.0, max_srb_trans=4.0,max_srb_rot=0.4)
+	self.mols=IMP.pmi.tools.get_molecules(self.hier)
+
     def test_rmsd_simple(self):
         """Test get_particle_infos_for_pdb_writing with no particles"""
-        rmsd=IMP.pmi.analysis.RMSD(hier,hier,[mol.get_name() for mol in mols],dynamic0=True,dynamic1=False)
+        rmsd=IMP.pmi.analysis.RMSD(self.hier,self.hier,[mol.get_name() for mol in self.mols],dynamic0=True,dynamic1=False)
         total_rmsd,best_assignments=rmsd.get_rmsd_and_assigments()
         self.assertEqual(total_rmsd,0.0)
 

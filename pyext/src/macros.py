ece9cd254a7cff11e5e7b9bc5df4f31196748934
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2017-09-28 18:28:35 +0200

    remove useless printout

--- pyext/src/macros.py
+++ pyext/src/macros.py
@@ -2433,7 +2433,6 @@ class AnalysisReplicaExchange(object):
         # set the list of proteins on the x axis
         sorted_tuple=sorted([(IMP.pmi.topology.PMIMoleculeHierarchy(mol).get_extended_name(),mol) for mol in mols])
         prot_list=zip(*sorted_tuple)[1]
-        print(prot_list)
 
         prot_listx = prot_list
         nresx = gap_between_components + \

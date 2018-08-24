04612c2954f97c6b1b4ba1ec62fa700cf6bed74c
Ben Webb <ben@salilab.org>
2018-08-24 14:55:57 -0700

    Hide IMP.pmi1 import deprecation warning

--- tools/setup_travis.sh
+++ tools/setup_travis.sh
@@ -39,7 +39,8 @@ cp -sr ${pmi_dir}/pyext/src pmi1
 cp pmi1.orig/__init__.py pmi1.orig/_version_check.py pmi1/
 
 # Also replace PMI1 examples, since some tests use data from them
-EXAMPLE_PATH=$(echo "import IMP.pmi1, sys; sys.stdout.write(IMP.pmi1.get_example_path('..'))" | python | tail -1)
+# Need to set log level to NONE to hide any deprecation warnings
+EXAMPLE_PATH=$(echo "import IMP; IMP.set_log_level(IMP.NONE); import IMP.pmi1, sys; sys.stdout.write(IMP.pmi1.get_example_path('..'))" | python)
 cd ${EXAMPLE_PATH}
 mv pmi1 pmi1.orig
 cp -sr ${pmi_dir}/examples pmi1

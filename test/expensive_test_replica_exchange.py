d936717f425d8b56ff2e1f8d3a4a2e98c72744f2
Ben Webb <ben@salilab.org>
2018-10-08 16:32:53 -0700

    Move fast tests out of expensive file
    "Expensive" tests don't contribute to codecov
    coverage. Since some of these are very fast,
    move them into separate "fast" Python files.

--- test/expensive_test_replica_exchange.py
+++ test/expensive_test_replica_exchange.py
@@ -12,20 +12,6 @@ import os.path
 
 class Tests(IMP.test.TestCase):
 
-    def test_serial(self):
-        """Test the _SerialReplicaExchange class"""
-        s = IMP.pmi.samplers._SerialReplicaExchange()
-        self.assertEqual(s.get_number_of_replicas(), 1)
-        ts = s.create_temperatures(100., 200., 45)
-        self.assertEqual(len(ts), 1)
-        self.assertAlmostEqual(ts[0], 100.0, delta=1e-6)
-        self.assertEqual(s.get_my_index(), 0)
-        self.assertEqual(s.get_friend_index(0), 0)
-        s.set_my_parameter("temp", ['foo', 'bar'])
-        self.assertEqual(s.get_my_parameter("temp"), ['foo', 'bar'])
-        self.assertEqual(s.get_friend_parameter("temp", 0), ['foo', 'bar'])
-        self.assertEqual(s.do_exchange(0, 0, 0), False)
-
     def test_macro(self):
         """setting up the representation
         PMI 1.0 representation. Creates two particles and

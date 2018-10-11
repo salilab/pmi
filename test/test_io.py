0971eba02ab0099bc3dda34dd7e0e6dea19f4605
Ben Webb <ben@salilab.org>
2018-10-11 16:11:20 -0700

    Improve tests of parse_dssp()

--- test/test_io.py
+++ test/test_io.py
@@ -5,8 +5,8 @@ import IMP.pmi.io
 
 class Tests(IMP.test.TestCase):
 
-    def test_parse_dssp(self):
-        """Test reading DSSP files"""
+    def test_parse_dssp_single_chain(self):
+        """Test reading DSSP files, single chain"""
         sses = IMP.pmi.io.parse_dssp(self.get_input_file_name('chainA.dssp'),
                                      'A')
         self.assertEqual(sorted(sses.keys()),sorted(['helix','beta','loop']))
@@ -15,6 +15,18 @@ class Tests(IMP.test.TestCase):
         self.assertEqual(len(sses['helix']),20)
         self.assertEqual(len(sses['beta']),3)
         self.assertEqual(len(sses['loop']),32)
+
+    def test_parse_dssp_multiple_chain(self):
+        """Test reading DSSP files, single chain"""
+        sses = IMP.pmi.io.parse_dssp(self.get_input_file_name('chainA.dssp'),
+                                     name_map={'A':'prot1'})
+        self.assertEqual(sorted(sses.keys()),sorted(['helix','beta','loop']))
+        self.assertEqual(sses['helix'][1][0],[100,126,'prot1'])
+        self.assertEqual(sses['helix'][-1][0], [443, 444, 'I'])
+        self.assertEqual(sses['beta'][0],[[76,78,'prot1'],[91,93,'prot1']])
+        self.assertEqual(len(sses['helix']),121)
+        self.assertEqual(len(sses['beta']),18)
+        self.assertEqual(len(sses['loop']),183)
 
 if __name__ == '__main__':
     IMP.test.main()

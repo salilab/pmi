dcba26314535d912297b1d4826135ef157e74970
Ben Webb <ben@salilab.org>
2020-08-10 13:09:25 -0700

    Remove obsolete code

--- src/TransformMover.cpp
+++ src/TransformMover.cpp
@@ -61,11 +61,6 @@ core::MonteCarloMoverResult TransformMover::do_propose() {
   xyzs_.resize(pixyzs_.size());
   rbts_.resize(pirbs_.size());  
   
-  //xyzc=bb....;
-  
-  //get_rotation_about_point(const Vector3D &point,
-  //                                               const Rotation3D &rotation)
-
   algebra::Vector3D translation = IMP::algebra::Vector3D(0,0,0);
 
   if (constr_==0){
@@ -111,42 +106,19 @@ core::MonteCarloMoverResult TransformMover::do_propose() {
        core::transform(d,tt);
   }
 
-  //for (unsigned int i=0;i<pirbs_.size();i++){
-  //       RigidBody d(get_model(), pirbs_[i]);
-  //       last_transformation_[i] = d.get_reference_frame().get_transformation_to();
-  //    algebra::Rotation3D rc =
-  //    r * d.get_reference_frame().get_transformation_to().get_rotation();
-  //    algebra::Transformation3D t(rc, translation);  
-         
-  //IMP_LOG_VERBOSE("proposed move " << t_ << std::endl);
-  //IMP_USAGE_CHECK(
-  //    d.get_coordinates_are_optimized(),
-  //    "Rigid body passed to TransformMover"
-  //        << " must be set to be optimized. particle: " << d->get_name());
-  //d.set_reference_frame(algebra::ReferenceFrame3D(t));
-
   return core::MonteCarloMoverResult(pis_, 1.0);
 }
 
 void TransformMover::do_reject() {
   not_accepted_++;
-  //RigidBody d(get_model(), pi_);
-  //d.set_reference_frame(algebra::ReferenceFrame3D(last_transformation_));
-  //last_transformation_ = algebra::Transformation3D();
   for (unsigned int i=0;i<pixyzs_.size();i++) {
        core::XYZ d(get_model(), pixyzs_[i]);
        d.set_coordinates(xyzs_[i]);
-       //core::transform(d,c_);       
-       //core::transform(d,t_.get_inverse());
-       //core::transform(d,c_.get_inverse());     
   }
 
   for (unsigned int i=0;i<pirbs_.size();i++){
       core::RigidBody d(get_model(), pirbs_[i]);
       d.set_reference_frame(algebra::ReferenceFrame3D(rbts_[i]));
-      //core::transform(d,c_);       
-      //core::transform(d,t_.get_inverse());
-      //core::transform(d,c_.get_inverse()); 
   }
   
 }

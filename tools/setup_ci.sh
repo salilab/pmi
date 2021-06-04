bc61a9e24192480b66f2239cab406e33f8e3ca68
Ben Webb <ben@salilab.org>
2021-06-04 10:25:29 -0700

    Drop old Travis/IMP logic

--- tools/setup_ci.sh
+++ tools/setup_ci.sh
@@ -1,24 +1,16 @@
 #!/bin/bash -e
 
-# Set up an environment to run tests under Travis CI (see ../.travis.yml)
+# Set up an environment to run CI tests, e.g. with GitHub Actions or Travis
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 python_version"
   exit 1
 fi
 
-cur_dir=$(pwd)
 python_version=$1
-temp_dir=$(mktemp -d)
-
-cd ${temp_dir}
 
 conda config --remove channels defaults  # get conda-forge, not main, packages
 conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib imp-nightly gxx_linux-64 eigen swig cmake
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 pip install pytest-cov coverage pytest-flake8
-
-cd ${cur_dir}
-
-rm -rf ${temp_dir}
